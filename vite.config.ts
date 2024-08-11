ee10213798a6844473f2242cb3a947d01375cf5a
SNEHA <speakingofsneha@gmail.com>
2024-08-11 16:30:25 +1000

    debugging vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
+// https://vitejs.dev/guide/static-deploy.html#github-pages 
 export default defineConfig({
-  base: "/anu-testing",
+  base: "/anu/",
   mode: "production",
   plugins: [react(), tsConfigPaths()],
   server: {
