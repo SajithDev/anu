1d414bbae967f2db9253eb65ae4bda10be7ac8ca
SNEHA <speakingofsneha@gmail.com>
2024-08-11 16:21:43 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/anu",
+  base: "/anu-testing",
   mode: "production",
   plugins: [react(), tsConfigPaths()],
   server: {
